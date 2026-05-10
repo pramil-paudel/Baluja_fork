125c11dc6e982482e53dfb9b61198359e0dfb516
pramil-paudel <ppaudel35@gmail.com>
2026-05-10 12:49:14 -0500

    Bash Added

---
+++ Baluja.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+#SBATCH -J BALUJA_STEG
+#SBATCH -o slurm-%j.out
+
+#SBATCH -p gpu
+#SBATCH --gres="gpu:v100:1"
+python3 updatedLenslessScript.py
